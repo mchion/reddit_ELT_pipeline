339526f0557404f46d4df05d49e43c2c781cc783
Michael Chion <michael.chion@gmail.com>
2023-01-17 21:34:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -343,7 +343,7 @@ with st.sidebar:
         This dashboard is designed as a mock user endpoint for a data engineering project.
         Details about the data engineering pipeline can be found in the [Github](https://github.com/mchion/reddit_serverless_stream) repository.
         
-        This is NOT a data science or machine learning project, but future improvements will focus on the ML and MLOps side of this project.
+        This is NOT a data science or ML project, but future deployments will focus more on the ML and MLOps aspects.
         
         *All data on this dashboard is active and constantly changing based on new incoming data. 
         New comments from the dataengineering subreddit are ingested to the database on an hourly basis.*
