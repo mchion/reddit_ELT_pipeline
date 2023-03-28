028b72a2182fc2775e3630c5437d578c4787cba1
Michael Chion <michael.chion@gmail.com>
2023-03-27 20:05:01 -0400

    Update ingest_to_S3.py

--- ingest_to_S3.py
+++ ingest_to_S3.py
@@ -1,24 +1,18 @@
-import os
+import json
 import praw
 import boto3
+import time
+import json
+import logging
 from dataclasses import dataclass
-
 
 @dataclass
 class reddit():
 
-    CLIENT_ID = os.environ.get("REDDIT_CLIENT_ID")
-    CLIENT_SECRET = os.environ.get("REDDIT_CLIENT_SECRET")
-    USER_AGENT = os.environ.get("REDDIT_USER_AGENT")
-
-@dataclass
-class aws():
-
-    ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
-    SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
-    SESSION_TOKEN = os.environ.get("AWS_SESSION_TOKEN")
-
-
+    CLIENT_ID = <ENTER YOUR CLIENT_ID>
+    CLIENT_SECRET = <ENTER YOUR CLIENT_SECRET>
+    USER_AGENT = <ENTER YOUR USER_AGENT>
+    
 def get_reddit_handler():
     """Initialize and connect to the PRAW Reddit API"""
     
@@ -32,12 +26,7 @@ def get_reddit_handler():
 def get_S3_connection():
     """Connect to AWS S3 bucket"""
     
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id = aws.ACCESS_KEY,
-        aws_secret_access_key = aws.SECRET_KEY,
-        aws_session_token = aws.SESSION_TOKEN
-    )
+    s3_client = boto3.client('s3')
     return s3_client
 
 def get_reddit_comments(reddit_handler):
@@ -46,21 +35,24 @@ def get_reddit_comments(reddit_handler):
     count = 1
     reddit_comments = []
     
-    for comment in r.subreddit("dataengineering").stream.comments(skip_existing=False):
+    for comment in reddit_handler.subreddit("dataengineering").stream.comments(skip_existing=False):
         
         if count > 100 or comment is None: 
             break
+        try:
+            d = {'c_id': comment.id,
+                'c_author': comment.author.name if comment else '',
+                'c_body': comment.body,
+                'c_time': comment.created_utc,
+                's_id': comment.submission.id,
+                's_author': comment.submission.author.name if comment else '',
+                's_title': comment.submission.title,
+                's_time': comment.submission.created_utc
+            }
+            reddit_comments.append(d)
+        except (AttributeError,NameError):
+            logging.warning('a comment import failed at %s'%time.strftime("%Y.%m.%d-%H%M%S"))
 
-        d = {'c_id': comment.id,
-            'c_author': comment.author.name,
-            'c_body': comment.body,
-            'c_time': comment.created_utc,
-            's_id': comment.submission.id,
-            's_author': comment.submission.author.name,
-            's_title': comment.submission.title,
-            's_time': comment.submission.created_utc
-        }
-        reddit_comments.append(d)
         count += 1
 
     return reddit_comments
@@ -68,21 +60,20 @@ def get_reddit_comments(reddit_handler):
 def save_to_S3(reddit_comments, s3_client):
     """Save reddit comments as JSON file in S3"""
 
-    filename = 
+    body = json.dumps(reddit_comments, indent=4)
+    bucket = 'reddit-de-comments'
+    filename = f'{time.strftime("%Y%m%d-%H%M%S")}.json'
     
     s3_client.put_object(
-        Body=json.dumps(reddit_comments),
-        Bucket='reddit_data_engineering',
+        Body=body,
+        Bucket=bucket,
         Key= filename
 )
-
-def main():
+    
+def lambda_handler(event, context):
     """Connect to reddit API and ingest comments as JSON file to an AWS S3 bucket."""
-
+    
     reddit_handler = get_reddit_handler()
     reddit_comments = get_reddit_comments(reddit_handler)
     s3_client = get_S3_connection()
     save_to_S3(reddit_comments, s3_client)
-
-if __name__ == '__main__':
-    main()
